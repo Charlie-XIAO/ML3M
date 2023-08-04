11f4431e65d3a252fc008c4f3c0a92e6da04f660
Charlie-XIAO <yx2436@nyu.edu>
2023-08-04 15:43:07 +0800

    change doc conf

--- doc/conf.py
+++ doc/conf.py
@@ -25,7 +25,7 @@ extensions = [
 ]
 
 templates_path = ["_templates"]
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = ["build", "Thumbs.db", ".DS_Store"]
 
 
 intersphinx_mapping = {
